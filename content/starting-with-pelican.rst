b7c698db905a56163604ac61a4b44734d3ae9278
harshul1610 <harshulrobo@gmail.com>
2016-07-28 20:15:36 +0530

    first commit with my mother

---
+++ content/starting-with-pelican.rst
@@ -0,0 +1,7 @@
+Starting with Pelican
+#####################
+:date: 2016-07-28 19:32
+:category: programming
+:tags: python, blog, rst, pelican
+:author: Harshul Jain
+:excerpt: This is an excerpt of my post.
---
+++ pelicanconf.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Harshul Jain'
+SITENAME = u'Harshul Jain'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+THEME = "pelican-themes/Flex"
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+SITETITLE = 'Harshul Jain'
+SITESUBTITLE = 'Software Developer | Hardware Hacker'
+SITEDESCRIPTION = 'Connecting the world with Python.'
+
+SOCIAL = (('facebook', 'https://www.facebook.com/harshul.jain.7'),
+         ('github', 'https://github.com/harshul1610'),
+         ('google','https://plus.google.com/u/0/111412901938755370336/'),
+         ('twitter', 'https://twitter.com/harshulrobo'),
+         ('envelope-o', 'harshulrobo@gmail.com'),)
+
+DISQUS_SITENAME = 'blog-harshuljain'
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
